298df7d0cfd34853f616ee7bb729778c97384d77
ErickFSilvaProg <erick.ferreira.prog@gmail.com>
2025-07-24 15:45:59 -0300

    java_completo_poo_prof_nelio, 24/07/2025(tarde)

--- src/secao_05_estrutura_condicional/Aula039_switch_case.java
+++ src/secao_05_estrutura_condicional/Aula039_switch_case.java
@@ -1,10 +1,34 @@
 package secao_05_estrutura_condicional;
 
+import java.util.Locale;
+import java.util.Scanner;
+
 public class Aula039_switch_case {
 	
 	public static void main(String[] args) {
 		
+		/*
+		 * Fazer um programa para ler um valor inteiro de 1 a 7.
+		 * Representando um dia da semana (sendo 1 = domingo e 2 = segunda e assim por diante).
+		 * Escrever na tela o dia da semana correspondente, conforme exemplos.
+		 */
 		
+		
+		// Bibliotecas:
+		Locale.setDefault(Locale.US);
+		
+		// Variáveis:
+		Scanner sc = new Scanner(System.in);
+		int numero;
+		
+		
+		// Programa
+		System.out.print("Digite o dia da Semana: ");
+		numero = sc.nextInt();
+		
+		
+		// Fecha Scanner:
+		sc.close();
 		
 	}
 
