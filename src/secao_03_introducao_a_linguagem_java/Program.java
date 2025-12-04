dbb0db4023f9a09561373037229ab14c9cbc0997
ErickFSilvaProg <erick.ferreira.prog@gmail.com>
2025-12-04 16:34:12 -0300

    java_completo_poo_prof_nelio, 04/12/2025(tarde)

--- src/secao_03_introducao_a_linguagem_java/Program.java
+++ src/secao_03_introducao_a_linguagem_java/Program.java
@@ -2,6 +2,7 @@ package secao_03_introducao_a_linguagem_java;
 
 public class Program {
 	
+	// Método principal de um programa em Java:
 	public static void main(String[] args) {
 		
 		System.out.println("Olá mundo!");
