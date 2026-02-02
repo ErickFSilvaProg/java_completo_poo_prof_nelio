2a45fe835809c20669381eb0c7f4e62e25547b17
ErickFSilvaProg <erick.ferreira.prog@gmail.com>
2026-02-02 11:54:18 -0300

    java_completo_poo_prof_nelio, 01/02/2026 (manhã)

---
+++ src/secao_03_introducao_liguagem_java/Aula024_PrimeiroProgramaJava.java
@@ -0,0 +1,11 @@
+package secao_03_introducao_liguagem_java;
+
+public class Aula024_PrimeiroProgramaJava {
+	
+	public static void main(String[] args) {
+		
+		System.out.println("Olá mundo!");
+		
+	}
+
+}
---
+++ src/secao_03_introducao_liguagem_java/Aula025_SintaxeSimplificadaJava25.java
@@ -0,0 +1,11 @@
+package secao_03_introducao_liguagem_java;
+
+public class Aula025_SintaxeSimplificadaJava25 {
+	
+	void main() {
+		
+		IO.println("Olá Java 25!");
+		
+	}
+	
+}
