597bfff5f16ae4ddfa657a5c1adc050e276fb0e6
ErickFSilvaProg <erick.ferreira.prog@gmail.com>
2026-02-03 12:55:39 -0300

    java_completo_poo_prof_nelio, 03/02/2026 (manhã)

---
+++ src/secao_03_introducao_liguagem_java/Aula29_VariaveisTiposBasicosEmJava.java
@@ -0,0 +1,70 @@
+package secao_03_introducao_liguagem_java;
+
+public class Aula29_VariaveisTiposBasicosEmJava {
+	
+	/**
+	 * 
+	 * → Variáveis:
+	 * 
+	 * 		É uma porção de memória (RAM) utilizada para armazenar dados durante a execução dos programas.
+	 * 
+	 * 
+	 * 		→ Sintaxe: [tipo] [nome] = [valor inicial]
+	 * 
+	 * 			• Exemplos: int idade = 25;
+	 * 					    double altura = 1.75;
+	 * 					    char sexo = 'F';
+	 * 
+	 * 
+	 * 		→ Uma variáavel possui:
+	 * 
+	 * 			• Nome (ou identificador)
+	 * 			• Tipo
+	 * 			• Valor
+	 * 			• Endereço
+	 * 
+	 */
+	
+	
+	/**
+	 * 
+	 * → Tipos primitivos em Java:
+	 * 
+	 * 		→ Tipos numéricos inteiros:
+	 * 
+	 * 			• Tipo		• Tamanho		• Valores							• Valor padrão
+	 * 
+	 * 			byte		8  bits			-128 a 127							0
+	 * 			short		16 bits			-32.768 a 32.767					0
+	 * 			int			32 bits			-2.147.483.648 a 2.147.483.647		0
+	 * 			long		64 bits			-9.223.372.036.854.775.808 a		0L
+	 * 									 	 9.223.372.036.854.775.807
+	 * 
+	 * 
+	 * 		→ Tipos numéricos com ponto flutuantes:
+	 * 
+	 * 			• Tipo		• Tamanho		• Valores							• Valor padrão
+	 * 
+	 * 			float		32 bits			-1,4024E-37 a 3,4028E+38			0.0f
+	 * 			double		64 bits			-4,94E-307 a 3,4028E+38				0.0
+	 * 
+	 * 
+	 * 		→ Um caractere Unicode:
+	 * 
+	 * 			• Tipo		• Tamanho		• Valores							• Valor padrão
+	 * 
+	 * 			char		16 bits			'\u0000' a '\uFFFF'					'\u0000'
+	 * 
+	 * 
+	 * 		→ Valor verdade:
+	 * 
+	 * 			• Tipo		• Tamanho		• Valores							• Valor padrão
+	 * 
+	 * 			boolean		1 bit			[false, true]						false
+	 * 
+	 * 
+	 * → String - Cadeia de caracteres (palavras ou textos).
+	 * 
+	 */
+
+}
